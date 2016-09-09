fbeba8d9d33badf52b58bd3a10c13c4a60310edb
p4vlos <nicolaou_paul@hotmail.com>
2016-09-09 17:38:15 +0300

    first commit

---
+++ DreamLister/Item+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Item+CoreDataClass.swift
+//  DreamLister
+//
+//  Created by Pavlos Nicolaou on 09/09/2016.
+//  Copyright © 2016 Pavlos Nicolaou. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+public class Item: NSManagedObject {
+
+}
---
+++ DreamLister/Store+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Store+CoreDataClass.swift
+//  DreamLister
+//
+//  Created by Pavlos Nicolaou on 09/09/2016.
+//  Copyright © 2016 Pavlos Nicolaou. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+public class Store: NSManagedObject {
+
+}
