2d980686e91341714ad0a03d460595685a7e6c49
p4vlos <nicolaou_paul@hotmail.com>
2016-09-12 13:49:28 +0300

    setting up main view controller

---
+++ DreamLister/ItemCell.swift
@@ -0,0 +1,18 @@
+//
+//  ItemCell.swift
+//  DreamLister
+//
+//  Created by Pavlos Nicolaou on 12/09/2016.
+//  Copyright © 2016 Pavlos Nicolaou. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+    
+    @IBOutlet weak var thumb: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var details: UILabel!
+
+}
